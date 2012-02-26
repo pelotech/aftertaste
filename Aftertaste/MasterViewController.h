f7eab47d75674bf25cb69c3a20ef097bc6058d52
Donald DeSantis <donalddesantis@gmail.com>
2012-02-25 21:21:12 -0800

    changed root view controller to gallery view, dismissing camera now, trying (unsuccessfullY) to set correct pageviewcontroller data source to the gallery view controller

--- Aftertaste/MasterViewController.h
+++ Aftertaste/MasterViewController.h
@@ -10,7 +10,7 @@
 
 #import <CoreData/CoreData.h>
 
-@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate, UIPageViewControllerDataSource>
 
 @property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
